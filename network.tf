918379961067cd98290e1fe91e02b8064b05af76
Aryan Agarwal <aryankush025@gmail.com>
2025-08-06 15:46:56 +0530

    Adds network infrastructure resources
    Adds the core networking infrastructure components including VPC, public/private/secure subnets, internet gateway, NAT gateway, and Elastic IP.
    
    This initial setup provides the foundation for deploying applications and services within a secure and scalable network environment.

--- network.tf
+++ network.tf
@@ -1,3 +1,8 @@
+# Data source to get availability zones
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -7,6 +12,99 @@ resource "aws_vpc" "main" {
   tags = {
     Name = "${var.prefix}-vpc"
   }
+}
+
+# Public Subnets (2)
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet1_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = format("%s-public-subnet-1", var.prefix)
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet2_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = format("%s-public-subnet-2", var.prefix)
+  }
+}
+
+# Private Subnets (2)
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet3_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = format("%s-private-subnet-1", var.prefix)
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet4_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = format("%s-private-subnet-2", var.prefix)
+  }
+}
+
+# Secure Subnets (2)
+resource "aws_subnet" "secure_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet5_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = format("%s-secure-subnet-1", var.prefix)
+  }
+}
+
+resource "aws_subnet" "secure_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet6_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = format("%s-secure-subnet-2", var.prefix)
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = format("%s-internet-gateway", var.prefix)
+  }
+}
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = format("%s-nat-eip", var.prefix)
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "main_nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = format("%s-nat-gateway", var.prefix)
+  }
+
+  depends_on = [aws_internet_gateway.main_igw]
 }
 
 # To get the VPC ID
